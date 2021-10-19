8573edb902dfc15f47712ae1cc164f87a116e80e
Noisefox <hkopmdgt@protonmail.ch>
2021-10-19 14:51:03 +0000

    Update user.js

--- user.js
+++ user.js
@@ -60,7 +60,7 @@ user_pref("network.cookie.cookieBehavior", 1);
 "
 ***/
 
-user_pref("network.cookie.lifetimePolicy", 1);
+user_pref("network.cookie.lifetimePolicy", 0);
 /* "cookies are deleted at the end of the session
 
         0 = Accept cookies normally
